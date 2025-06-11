800c6dd4d0e314a7734c14d8849ac50c482213fb
nikanika156 <noobmaster156@gmail.com>
2025-06-11 23:06:33 +0400

    უპ

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { ICards } from "../../types/types";
 
 import { CardWithImage } from "../card/card-with-image/card-with-image";
@@ -10,64 +10,41 @@ interface Props {
 }
 
 export function Cards({ cards }: Props) {
-  const [loadedCount, setLoadedCount] = useState(0);
   const [loading, setLoading] = useState(true);
-  const [inCache, setInCache] = useState(false);
-  const cancelled = useRef(false);
-  // set loading
-  // useEffect(() => {
-  //   if (cards.length && Math.min(loadedCount, cards.length) == cards.length) {
-  //     setTimeout(() => {
-  //       // loading.current = false;
-  //       setLoading(false);
-  //     }, 250);
-  //   } else {
-  //     // loading.current = true;
-  //     setLoading(true);
-  //   }
-  //   console.log(loadedCount);
-  // }, [loadedCount]);
-  //
-  // Preload images
-  //
-  const preloadImage = async () => {
-    let cached = false;
-    let loaded = false;
-    let countInCache = 0;
-    setLoadedCount(0);
-    setLoading(true);
 
-    cards.map(({ image }) => {
-      const xml = new XMLHttpRequest();
-      xml.open("GET", image);
-      xml.responseType = "blob";
-
-      // if (xml.) {
-      xml.onloadend = (x) => {
-        console.log((countInCache += 1));
-      };
-      xml.onload = function(e){
-        console.log(e);
-        
-      }
-      // } else {
-      //   xml.onload = () => {
-      //     setLoadedCount((prev) => prev + 1);
-      //   };
-      // }
+  const preloadPromise = (image: string) => {
+    return new Promise<string>((resolve) => {
+      const img = new Image();
+      img.src = image;
+      if (img.complete) resolve("cache");
+      else
+        img.onload = () => {
+          resolve("loaded");
+        };
     });
-
-    return {
-      cached: cached,
-      loaded: loaded,
-    };
   };
-  useEffect(() => {
-    preloadImage().then((x) => {
-      console.log(x);
-    });
-  }, [cards]);
 
+  const preloadImage = async () => {
+    setLoading(true);
+    const results = await Promise.all(
+      cards.map(({ image }) => preloadPromise(image)),
+    );
+    results.forEach((status) => console.log(status));
+    return results;
+  };
+
+  useEffect(() => {
+    if (cards.length)
+      preloadImage().then((x) => {
+        if (x.find((x) => x == "loaded")) {
+          setTimeout(() => {
+            setLoading(false);
+          }, 250);
+        } else if (x.every((x) => x == "cache") && x.length >= cards.length) {
+          setLoading(false);
+        }
+      });
+  }, [cards]);
   return (
     <>
       <div className="w-full border-t-1 border-t-stone-500! py-10">
@@ -81,7 +58,7 @@ export function Cards({ cards }: Props) {
                   name={card.name}
                   origin={card.origin.name}
                   species={card.species}
-                  image={card.image}
+                  image={!loading && card.image}
                 />
               )}
             </li>
